8116f2d500b697a9ba7f7cd135d14ce56bb11439
Alex Baker <al3xb4k3r@gmail.com>
2014-11-23 15:36:04 -0700

    moved and renamed files, added copy task for images directory, made custom task to delete js file if the file is empty

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,35 +10,53 @@ module.exports = function (grunt) {
       },
       single_file: {
         src: 'static/styles/main.css',
-        dest: 'static/styles/main.prefixed.css'
+        dest: 'dist/main.min.css'
       }
     },
     concat: {
-      dist: {
-        src: ['static/js/vendor/*.js', 'static/js/main.js'],
-        dest: 'static/js/main.built.js'
+      js: {
+        src: ['static/js/modules/*.js', 'static/js/main.js'],
+        dest: 'dist/main.min.js'
+      },
+      css: {
+        src: ['static/styles/normalize.css', 'static/styles/main.css'],
+        dest: 'dist/main.min.css'
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        flatten: true,
+        src: 'static/img/*',
+        dest: 'dist/img',
+        filter: 'isFile'
       }
     },
     cssmin : {
       css: {
-        src: 'static/styles/main.prefixed.css',
-        dest: 'static/styles/main.prefixed.min.css'
+        src: 'dist/main.min.css',
+        dest: 'dist/main.min.css'
       }
     },
-    sass: {
+    processhtml: {
       dist: {
-        options: {
-          style: 'expanded'
-        },
         files: {
-          'static/styles/main.css': 'static/styles/sass/main.scss'
+          'dist/index.html': ['index.html']
         }
       }
     },
+    sass: {
+      options: {
+        style: 'expanded'
+      },
+      files: {
+        'static/styles/main.css': 'static/styles/sass/main.scss'
+      }
+    },
     uglify : {
-      js: {
+      my_target: {
         files: {
-          'static/js/main.built.min.js' : ['static/js/main.built.js']
+          'dist/main.min.js' : ['dist/main.min.js']
         }
       }
     },
@@ -66,13 +84,32 @@ module.exports = function (grunt) {
   });
   grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-processhtml');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // tasks
+  // custom tasks
+  grunt.registerTask('checkJS', function () {
+    var jsFile;
+    jsFile = grunt.file.read('dist/main.min.js');
+    if (jsFile === "") {
+      grunt.file.delete('dist/main.min.js');
+    }
+  });
+
+  // main tasks
   grunt.registerTask('dev', ['watch']);
-  grunt.registerTask('pro', ['sass', 'autoprefixer', 'cssmin', 'concat', 'uglify']);
+  grunt.registerTask('pro', [
+    'sass',
+    'autoprefixer',
+    'concat',
+    'cssmin',
+    'uglify',
+    'copy',
+    'processhtml',
+    'checkJS'
+  ]);
 };
